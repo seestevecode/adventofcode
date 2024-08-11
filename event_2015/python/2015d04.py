393f6d26383b5a09f258e2ab643649a5490701ef
Steve Hill <code@seesteve.xyz>
2024-08-11 19:59:45 +0100

    Add 2015 day 4 for Python

---
+++ event_2015/python/2015d04.py
@@ -0,0 +1,30 @@
+import hashlib
+
+# import input
+input = "ckczppom"
+
+
+def find_lowest_num(start_string):
+    num = 1
+    while (
+        not hashlib.md5((input + str(num)).encode())
+        .hexdigest()
+        .startswith(start_string)
+    ):
+        num += 1
+    return num
+
+
+# part 1
+def part_one():
+    return find_lowest_num("00000")
+
+
+# part 2
+def part_two():
+    return find_lowest_num("000000")
+
+
+# results
+print("Part 1:", part_one())  # 117946
+print("Part 2:", part_two())  # 3938038
